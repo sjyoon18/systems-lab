bf74e429012943a9b726f4d79fb07f0b3f74375b
sjyoon18 <seojuny1008@gmail.com>
2026-06-15 17:49:27 +0900

    Implement concurrent HTTP file server with HTML routing

--- networking/http_server.c
+++ networking/http_server.c
@@ -3,8 +3,57 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <signal.h>
+#include<sys/wait.h>
+
+void reap_children(int sig) {
+    while(waitpid(-1, NULL, WNOHANG) > 0) {
+    }
+}
+
+void send_file_response(int client_fd, char *file_path) {
+    char body[4096];
+    
+    int file_fd = open(file_path, O_RDONLY);
+
+    if (file_fd < 0) {
+        char *not_found =
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 14\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "404 Not Found\n";
+
+        write(client_fd, not_found, strlen(not_found));
+        return;
+    }
+
+    int body_len = read(file_fd, body, sizeof(body));
+    
+    close(file_fd);
+
+    char header[256];
+    int header_len = snprintf(
+        header,
+        sizeof(header),
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Content-Length: %d\r\n"
+        "Connection: close\r\n"
+        "\r\n",
+        body_len
+    );
+
+    write(client_fd, header, header_len);
+    write(client_fd, body, body_len);
+}
 
 int main() {
+    signal(SIGCHLD, reap_children);
+
     int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in addr;
@@ -25,61 +74,42 @@ int main() {
     while(1) {
         int client_fd = accept(listen_fd, NULL, NULL);
 
-        char buffer[4096];
+        pid_t pid = fork();
 
-        int n = read(client_fd, buffer, sizeof(buffer));
+        if (pid == 0) {
+            close(listen_fd);
 
-        printf("\n[server] client CONNECTED\n");
+             char buffer[4096];
 
-        printf("\n-------REQUEST-------\n");
-        write(1, buffer, n);
-        printf("\n---------------------\n");
+            int n = read(client_fd, buffer, sizeof(buffer));
 
-        char *response;
+            printf("\n[server] client CONNECTED\n");
 
-        if (strncmp(buffer, "GET / ", 6) == 0) {
-            response =
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/plain\r\n"
-            "Content-Length: 12\r\n"
-            "Connection: close\r\n"
-            "\r\n"
-            "Hello World\n";
-        }
-        else if (strncmp(buffer, "GET /cats ", 10) == 0) {
-            response =
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/plain\r\n"
-            "Content-Length: 11\r\n"
-            "Connection: close\r\n"
-            "\r\n"
-            "Hello Cats\n";
-        }
-        else if (strncmp(buffer, "GET /dogs ", 10) == 0) {
-            response =
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/plain\r\n"
-            "Content-Length: 11\r\n"
-            "Connection: close\r\n"
-            "\r\n"
-            "Hello Dogs\n";
+            printf("-------REQUEST-------\n");
+            write(1, buffer, n);
+            printf("\n---------------------\n");
+
+            if (strncmp(buffer, "GET / ", 6) == 0) {
+                send_file_response(client_fd, "www/index.html");
+            }
+            else if (strncmp(buffer, "GET /cats ", 10) == 0) {
+                send_file_response(client_fd, "www/cats.html");
+            }
+            else if (strncmp(buffer, "GET /dogs ", 10) == 0) {
+                send_file_response(client_fd, "www/dogs.html");
+            }
+            else {
+                send_file_response(client_fd, "nofile");
+            }
+
+            close(client_fd);
+            exit(0);
         }
         else {
-            response = 
-            "HTTP/1.1 404 Not Found\r\n"
-            "Content-Type: text/plain\r\n"
-            "Content-Length: 14\r\n"
-            "Connection: close\r\n"
-            "\r\n"
-            "404 Not Found\n";
+            close(client_fd);
         }
-
-        write(client_fd, response, strlen(response));
-
-        close(client_fd);
     }
 
     close(listen_fd);
-
     return 0;
 }
