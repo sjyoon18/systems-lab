20632d1aca5eda7e6f5efb732df44b9cab3a19a8
sjyoon18 <seojuny1008@gmail.com>
2026-06-25 15:31:23 +0900

    Build initial linked-list memory allocator

---
+++ mini_malloc/mini_malloc.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+
+struct block {
+    size_t size;
+    int free;
+    struct block *next;
+};
+
+struct block *head = NULL;
+
+struct block* find_free_block(size_t size) {
+    struct block *current = head;
+
+    while (current != NULL) {
+        if (current->free == 1 && current->size >= size) {
+            return current;
+        }
+        current = current->next;
+    }
+
+    return NULL;
+}
+
+void *my_malloc(size_t size) {
+    if (size == 0) {
+        return NULL;
+    }
+
+    struct block *free_block = find_free_block(size);
+
+    if (free_block != NULL) {
+        free_block->free = 0;
+
+        return free_block + 1;
+    }
+
+    void *raw = sbrk(sizeof(struct block) + size);
+
+    if (raw == (void *)-1) {
+        return NULL;
+    }
+
+    struct block *metadata = raw;
+
+    metadata->size = size;
+    metadata->free = 0;
+    metadata->next = head;
+
+    head = metadata;
+
+    return metadata + 1;
+}
+
+void my_free(void *ptr) {
+    if (ptr == NULL) {
+        return;
+    }
+    
+    struct block *metadata = (struct block *)ptr - 1;
+    metadata->free = 1;
+}
+
+int main() {
+    char *p1 = my_malloc(10);
+    char *p2 = my_malloc(20);
+
+    printf("p1 = %p\n", p1);
+    printf("p2 = %p\n", p2);
+
+    my_free(p1);
+
+    char *p3 = my_malloc(5);
+
+    printf("p3 = %p\n", p3);
+
+    return 0;
+}
