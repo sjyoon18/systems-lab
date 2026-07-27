c4a5d85ce26532ffb2c5c6cce3ea80bf437a16a2
sjyoon18 <seojuny1008@gmail.com>
2026-07-27 16:50:30 +0900

    Update hashmap public API

--- hashmap/hashmap.h
+++ hashmap/hashmap.h
@@ -2,11 +2,14 @@
 #define HASHMAP_H
 
 #include <stddef.h>
+#include <stdbool.h>
 
 struct hashmap;
 
 struct hashmap *hashmap_create(size_t bucket_count);
+bool hashmap_put(struct hashmap *map, const char *key, void *value);
 void *hashmap_get(struct hashmap *map, const char *key);
+bool hashmap_remove(struct hashmap *map, const char *key);
 void hashmap_destroy(struct hashmap *map);
 
 #endif
